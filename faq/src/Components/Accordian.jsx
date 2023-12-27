2741baa9826ad92cd338ca8e26440b5326015af4
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-27 12:02:58 +0530

    add

--- faq/src/Components/Accordian.jsx
+++ faq/src/Components/Accordian.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import plus from '../assets/images/icon-plus.svg';
+import React, { useState } from "react";
+import plus from "../assets/images/icon-plus.svg";
+import minus from "../assets/images/icon-minus.svg";
 export default function Accordian(props) {
+  const [open, setOpen] = useState(false);
   return (
     <section className="acc">
-        <div className="question flex flex-row justify-between text-[#2f1533] font-bold">
-            <h1 className='text-[1rem] hover:text-[#b256d4] cursor-pointer'>{props.question}</h1>
-            <button><img src={plus} alt="plus" /></button>
-        </div>
-        <div className="ans text-[0.8rem] text-[#8c6991]">
-            <p>{props.answer}</p>
-        </div>
+      <div className="question flex flex-row justify-between text-[#2f1533] font-bold">
+        <h1 className="text-[1rem] hover:text-[#b256d4] cursor-pointer">
+          {props.question}
+        </h1>
+        <button
+          className="cursor-pointer hover:text-[#ffffff] transition-all duration-300 ease-linear"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+            {
+                open?<img className="cursor-pointer  transition-all duration-300 ease-linear" src={minus} alt="plus" />:<img className="cursor-pointer  transition-all duration-300 ease-linear" src={plus} alt="plus" />
+            }
+          
+        </button>
+      </div>
+      <div className={`ans text-[0.8rem] text-[#8c6991] anidown ${open?"block":"hidden"} transition-all duration-500 ease-linear`}>
+        <p>{props.answer}</p>
+      </div>
     </section>
-  )
+  );
 }
