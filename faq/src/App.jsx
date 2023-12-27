8c44b4dd95a39b5219624c77dd07f4288b3254af
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-27 15:09:50 +0530

    attribution

--- faq/src/App.jsx
+++ faq/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 import './App.css'
 import Faqs from './Components/Faqs'
+import Attribution from './Components/Attribution'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen">
       <Faqs/>
+      <Attribution/>
       </div>
     </>
   )
---
+++ faq/src/Components/Attribution.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Attribution() {
+  return (
+    <div className="attribution font-bold m-[1rem] text-black">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">IndranjanaChatterjee</a>.
+  </div>
+  )
+}
